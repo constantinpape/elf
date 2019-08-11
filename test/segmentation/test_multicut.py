d8a919ca71890bb41e3d4240d828f60a7b589746
Constantin Pape <c.pape@gmx.net>
2019-08-11 23:02:06 +0200

    Update multicut tests

--- test/segmentation/test_multicut.py
+++ test/segmentation/test_multicut.py
@@ -1,14 +1,34 @@
 import unittest
 import numpy as np
 try:
-    import nifty
+    import nifty.graph.opt.multicut as nmc
 except ImportError:
     nifty = None
 
 
 class TestMulticut(unittest.TestCase):
+    # TODO download from Paul's website instead of hard-coding
+    problem_root = '/home/pape/Work/data/multicut_problems/small/small_problem_sampleA.txt'
+    upper_bound = -76900
 
-    # cf https://github.com/constantinpape/graph/blob/master/src/andres/graph/unit-test/multicut/kernighan-lin.cxx
+    @staticmethod
+    def load_problem(path):
+        edges = []
+        costs = []
+        with open(path, 'r') as f:
+            for l in f:
+                r = l.split()
+                edges.append([int(r[0]), int(r[1])])
+                costs.append(float(r[2]))
+        edges = np.array(edges, dtype='uint64')
+        edges = np.sort(edges, axis=1)
+        costs = np.array(costs)
+        n_nodes = int(edges.max()) + 1
+        graph = nifty.graph.undirectedGraph(n_nodes)
+        graph.insertEdges(edges)
+        return graph, costs
+
+    # https://github.com/constantinpape/graph/blob/master/src/andres/graph/unit-test/multicut/kernighan-lin.cxx
     def toy_problem(self):
         graph = nifty.graph.undirectedGraph(6)
         edges = np.array([[0, 1],
@@ -23,34 +43,65 @@ class TestMulticut(unittest.TestCase):
         expected_result = np.array([0, 1, 0, 1, 1, 0, 0], dtype='bool')  # edge result
         return graph, costs, expected_result
 
-    def _test_multicut(self, solver):
+    def _test_multicut_toy(self, solver):
         graph, costs, expected_result = self.toy_problem()
         node_labels = solver(graph, costs)
         uv_ids = graph.uvIds()
         result = node_labels[uv_ids[:, 0]] != node_labels[uv_ids[:, 1]]
         self.assertTrue(np.array_equal(result, expected_result))
 
-    @unittest.skipUnless(nifty, "Need nifty for multicut functionality")
+    def _test_multicut(self, solver):
+        # TODO remove the try-except once download is implemented
+        try:
+            graph, costs = self.load_problem()
+        except FileNotFoundError:
+            return
+        node_labels = solver(graph, costs)
+        obj = nmc.multicutObjective(graph, costs)
+        energy = obj.evalNodeLabels(node_labels)
+        self.assertGreater(self.upper_bound, energy)
+
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
     def test_gaec(self):
         from elf.segmentation.multicut import multicut_gaec
         self._test_multicut(multicut_gaec)
 
-    @unittest.skipUnless(nifty, "Need nifty for multicut functionality")
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
+    def test_gaec_toy(self):
+        from elf.segmentation.multicut import multicut_gaec
+        self._test_multicut_toy(multicut_gaec)
+
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
     def test_kernighan_lin(self):
         from elf.segmentation.multicut import multicut_kernighan_lin
         self._test_multicut(multicut_kernighan_lin)
 
-    @unittest.skipUnless(nifty, "Need nifty for multicut functionality")
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
+    def test_kernighan_lin_toy(self):
+        from elf.segmentation.multicut import multicut_kernighan_lin
+        self._test_multicut_toy(multicut_kernighan_lin)
+
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
     def test_decomposition(self):
         from elf.segmentation.multicut import multicut_decomposition
         self._test_multicut(multicut_decomposition)
 
-    @unittest.skipUnless(nifty, "Need nifty for multicut functionality")
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
+    def test_decomposition_toy(self):
+        from elf.segmentation.multicut import multicut_decomposition
+        self._test_multicut_toy(multicut_decomposition)
+
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
     def test_fusion_moves(self):
         from elf.segmentation.multicut import multicut_fusion_moves
         self._test_multicut(multicut_fusion_moves)
 
-    @unittest.skipUnless(nifty, "Need nifty for multicut functionality")
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
+    def test_fusion_moves_toy(self):
+        from elf.segmentation.multicut import multicut_fusion_moves
+        self._test_multicut_toy(multicut_fusion_moves)
+
+    @unittest.skipUnless(nmc, "Need nifty for multicut functionality")
     def test_transform_probabilities_to_costs(self):
         from elf.segmentation.multicut import transform_probabilities_to_costs
         probs = np.random.rand(100).astype('float32')
