ce9e61c4ca7de569ae32e5414206c7b4acf35ad5
Constantin Pape <c.pape@gmx.net>
2020-01-19 19:00:49 +0100

    Update affine prototype impl

--- elf/transformation/affine_impl.py
+++ elf/transformation/affine_impl.py
@@ -3,11 +3,8 @@ import nifty.tools as nt
 from .affine import transform_coordinate
 
 
-# TODO handle the out of range coordinates
-
-
 def apply_affine_2d(data, out, blocking, matrix, start, stop):
-    # TODO in a more serious impl, would need to limit the cache size,$
+    # TODO in a more serious impl, would need to limit the cache size,
     # ideally using lru cache
     chunk_cache = {}
     chunks = blocking.blockShape
@@ -16,8 +13,12 @@ def apply_affine_2d(data, out, blocking, matrix, start, stop):
         for jj in range(start[1], stop[1]):
             old_coord = (ii, jj)
             coord = transform_coordinate(old_coord, matrix)
-            # TODO support more then nearest neighbor assignment
+            # TODO support more than nearest neighbor assignment
             coord = [int(round(co, 0)) for co in coord]
+
+            # range check
+            if any(co < 0 for co in coord) or any(co >= sh for co, sh in zip(coord, data.shape)):
+                continue
 
             chunk_id = blocking.coordinatesToBlockId(coord)
             chunk, offset = chunk_cache.get(chunk_id, (None, None))
@@ -37,17 +38,23 @@ def apply_affine_2d(data, out, blocking, matrix, start, stop):
 
 
 def apply_affine_3d(data, out, blocking, matrix, start, stop):
-    # TODO in a more serious impl, would need to limit the cache size,$
+    # TODO in a more serious impl, would need to limit the cache size
     # ideally using lru cache
     chunk_cache = {}
+    chunks = blocking.blockShape
 
     for ii in range(start[0], stop[0]):
         for jj in range(start[1], stop[1]):
             for kk in range(start[2], stop[2]):
                 old_coord = (ii, jj, kk)
                 coord = transform_coordinate(old_coord, matrix)
-                # TODO support more then nearest neighbor assignment
+
+                # TODO support more than nearest neighbor assignment
                 coord = tuple(int(round(co, 0)) for co in coord)
+
+                # range check
+                if any(co < 0 for co in coord) or any(co >= sh for co, sh in zip(coord, data.shape)):
+                    continue
 
                 chunk_id = blocking.coordinatesToBlockId(coord)
                 chunk, offset = chunk_cache.get(chunk_id, None)
@@ -55,30 +62,15 @@ def apply_affine_3d(data, out, blocking, matrix, start, stop):
                     # NOTE only works for z5py, need to implement this for other backends
                     # also, it's problematic to put this into c++ if we want to support arbitrary
                     # backends, try numba / cython first?
-                    chunk = data.read_chunk(chunk_id)
-                    offset = blocking.blockGridPosition(chunk_id)
+                    chunk_pos = blocking.blockGridPosition(chunk_id)
+                    chunk = data.read_chunk(chunk_pos)
+                    offset = [cp * ch for cp, ch in zip(chunk_pos, chunks)]
                     chunk_cache[chunk_id] = (chunk, offset)
 
-                chunk_coord = tuple(oc - of for oc, of in zip(old_coord, offset))
+                chunk_coord = tuple(oc - of for oc, of in zip(coord, offset))
                 val = chunk[chunk_coord]
                 out[old_coord] = val
     return out
-
-
-def apply_affine_2d_noc(data, out, matrix, start, stop):
-    for ii in range(start[0], stop[0]):
-        for jj in range(start[1], stop[1]):
-            old_coord = (ii, jj)
-            coord = transform_coordinate(old_coord, matrix)
-            # TODO support more then nearest neighbor assignment
-            coord = tuple(int(round(co, 0)) for co in coord)
-            val = data[coord]
-            out[old_coord] = val
-    return out
-
-
-def apply_affine_3d_noc(data, out, matrix, start, stop):
-    pass
 
 
 def apply_with_chunks(data, matrix, out, chunks, start, stop):
@@ -94,29 +86,14 @@ def apply_with_chunks(data, matrix, out, chunks, start, stop):
     return out
 
 
-def apply_no_chunks(data, matrix, out, start, stop):
-    ndim = data.ndim
-    if ndim == 2:
-        out = apply_affine_2d_noc(data, out, matrix, start, stop)
-    elif ndim == 3:
-        out = apply_affine_3d_noc(data, out, matrix, start, stop)
-    else:
-        raise ValueError
-    return out
-
-
 # prototype impl for on the fly affine transformation of
 # sub-volumes / bounding boxes
 def apply_affine_for_subvolume(data, matrix, bb):
     chunks = getattr(data, 'chunks', None)
-
+    assert chunks is not None
     start = tuple(b.start for b in bb)
     stop = tuple(b.stop for b in bb)
     sub_shape = tuple(sto - sta for sta, sto in zip(start, stop))
-    out = np.zeros(sub_shape)
-
-    if chunks is None:
-        out = apply_no_chunks(data, matrix, out, start, stop)
-    else:
-        out = apply_with_chunks(data, matrix, out, chunks, start, stop)
+    out = np.zeros(sub_shape, dtype=data.dtype)
+    out = apply_with_chunks(data, matrix, out, chunks, start, stop)
     return out
---
+++ example/transformation/affine.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import z5py
+
+from skimage.data import astronaut
+from scipy.ndimage import affine_transform
+from elf.transformation.affine import compute_affine_matrix
+from elf.transformation.affine_impl import apply_affine_for_subvolume
+
+
+def make_input():
+    im = astronaut()[:, :, 0]
+    print(im.shape)
+    f = z5py.File('data.n5')
+    f.create_dataset('astronaut', data=im, chunks=(64, 64))
+
+
+def check_affine():
+
+    f = z5py.File('data.n5')
+    ds = f['astronaut']
+
+    im = ds[:]
+    slice_ = np.s_[0:256, 0:256]
+
+    matrix = compute_affine_matrix(scale=(.5, .5), rotation=(45,))
+    matrix = np.linalg.inv(matrix)
+    reference = affine_transform(im, matrix, order=0)[slice_]
+
+    transformed = apply_affine_for_subvolume(ds, matrix, slice_)
+
+    if np.allclose(reference, transformed):
+        print("Results agree")
+    else:
+        print("Results DO NOT agree")
+
+    fig, ax = plt.subplots(2)
+    ax[0].imshow(reference, cmap='gray')
+    ax[1].imshow(transformed, cmap='gray')
+    plt.show()
+
+
+# make_input()
+check_affine()
