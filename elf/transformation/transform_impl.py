7c266c2d052c29867c8dd89b5c4de31ee770a705
Constantin Pape <c.pape@gmx.net>
2020-01-21 17:04:31 +0100

    Implement coordinate interpolation mechanism for elf.transformation

--- elf/transformation/transform_impl.py
+++ elf/transformation/transform_impl.py
@@ -8,8 +8,9 @@ import nifty.tools as nt
 #     import vigra.filters as ff
 
 
-def apply_transform_chunked_2d(data, out, transform_coordinate, start, stop,
-                               order, blocking, sigma):
+def apply_transform_chunked_2d(data, out,
+                               transform_coordinate, sample_coordinates,
+                               start, stop, blocking, sigma):
     # TODO in a more serious impl, would need to limit the cache size,
     # ideally using lru cache
     chunk_cache = {}
@@ -30,30 +31,35 @@ def apply_transform_chunked_2d(data, out, transform_coordinate, start, stop,
             if any(co < 0 or co >= maxr for co, maxr in zip(coord, max_range)):
                 continue
 
-            # TODO support more than nearest neighbor assignment
-            coord = [int(round(co, 0)) for co in coord]
+            # get the coordinates to iterate over and the interpolation weights
+            coords, weights = sample_coordinates(coord)
 
-            chunk_id = blocking.coordinatesToBlockId(coord)
-            chunk, offset = chunk_cache.get(chunk_id, (None, None))
-            if chunk is None:
-                # NOTE only works for z5py, need to implement this for other backends
-                # also, it's problematic to put this into c++ if we want to support arbitrary
-                # backends, try numba / cython first?
-                chunk_pos = blocking.blockGridPosition(chunk_id)
-                chunk = data.read_chunk(chunk_pos)
-                # TODO apply pre-smoothing to chunk if sigma is not None
-                offset = [cp * ch for cp, ch in zip(chunk_pos, chunks)]
-                chunk_cache[chunk_id] = (chunk, offset)
+            # iterate over coordinates and compute the output value
+            val = 0.
+            for coord, weight in zip(coords, weights):
+                chunk_id = blocking.coordinatesToBlockId(list(coord))
+                chunk, offset = chunk_cache.get(chunk_id, (None, None))
+                if chunk is None:
+                    # NOTE only works for z5py, need to implement this for other backends
+                    # also, it's problematic to put this into c++ if we want to support arbitrary
+                    # backends, try numba / cython first?
+                    chunk_pos = blocking.blockGridPosition(chunk_id)
+                    chunk = data.read_chunk(chunk_pos)
+                    # TODO apply pre-smoothing to chunk if sigma is not None
+                    offset = [cp * ch for cp, ch in zip(chunk_pos, chunks)]
+                    chunk_cache[chunk_id] = (chunk, offset)
 
-            chunk_coord = tuple(oc - of for oc, of in zip(coord, offset))
-            val = chunk[chunk_coord]
+                chunk_coord = tuple(oc - of for oc, of in zip(coord, offset))
+                val += weight * chunk[chunk_coord]
+
             out_coord = tuple(co - of for co, of in zip(old_coord, start))
             out[out_coord] = val
     return out
 
 
-def apply_transform_chunked_3d(data, out, transform_coordinate, start, stop,
-                               order, blocking, sigma):
+def apply_transform_chunked_3d(data, out,
+                               transform_coordinate, sample_coordinates,
+                               start, stop, blocking, sigma):
     # TODO in a more serious impl, would need to limit the cache size
     # ideally using lru cache
     chunk_cache = {}
@@ -72,29 +78,34 @@ def apply_transform_chunked_3d(data, out, transform_coordinate, start, stop,
                 if any(co < 0 or co >= maxr for co, maxr in zip(coord, max_range)):
                     continue
 
-                # TODO support more than nearest neighbor assignment
-                coord = [int(round(co, 0)) for co in coord]
+                # get the coordinates to iterate over and the interpolation weights
+                coords, weights = sample_coordinates(coord)
 
-                chunk_id = blocking.coordinatesToBlockId(coord)
-                chunk, offset = chunk_cache.get(chunk_id, (None, None))
-                if chunk is None:
-                    # NOTE only works for z5py, need to implement this for other backends
-                    # also, it's problematic to put this into c++ if we want to support arbitrary
-                    # backends, try numba / cython first?
-                    chunk_pos = blocking.blockGridPosition(chunk_id)
-                    chunk = data.read_chunk(chunk_pos)
-                    # TODO apply pre-smoothing to chunk if sigma is not None
-                    offset = [cp * ch for cp, ch in zip(chunk_pos, chunks)]
-                    chunk_cache[chunk_id] = (chunk, offset)
+                # iterate over coordinates and compute the output value
+                val = 0.
+                for coord, weight in zip(coords, weights):
+                    chunk_id = blocking.coordinatesToBlockId(list(coord))
+                    chunk, offset = chunk_cache.get(chunk_id, (None, None))
+                    if chunk is None:
+                        # NOTE only works for z5py, need to implement this for other backends
+                        # also, it's problematic to put this into c++ if we want to support arbitrary
+                        # backends, try numba / cython first?
+                        chunk_pos = blocking.blockGridPosition(chunk_id)
+                        chunk = data.read_chunk(chunk_pos)
+                        # TODO apply pre-smoothing to chunk if sigma is not None
+                        offset = [cp * ch for cp, ch in zip(chunk_pos, chunks)]
+                        chunk_cache[chunk_id] = (chunk, offset)
 
-                chunk_coord = tuple(oc - of for oc, of in zip(coord, offset))
-                val = chunk[chunk_coord]
+                    chunk_coord = tuple(oc - of for oc, of in zip(coord, offset))
+                    val += weight * chunk[chunk_coord]
                 out_coord = tuple(co - of for co, of in zip(old_coord, start))
                 out[out_coord] = val
     return out
 
 
-def apply_transform_2d(data, out, transform_coordinate, start, stop, order):
+def apply_transform_2d(data, out,
+                       transform_coordinate, sample_coordinates,
+                       start, stop):
 
     # precompute for range check
     max_range = tuple(sh - 1 for sh in data.shape)
@@ -108,16 +119,19 @@ def apply_transform_2d(data, out, transform_coordinate, start, stop, order):
             if any(co < 0 or co >= maxr for co, maxr in zip(coord, max_range)):
                 continue
 
-            # TODO support more than nearest neighbor assignment
-            coord = tuple(int(round(co, 0)) for co in coord)
+            # get the coordinates to iterate over and the interpolation weights
+            coords, weights = sample_coordinates(coord, where_format=True)
+            # compute the output value
+            val = (weights * data[coords]).sum()
 
-            val = data[coord]
             out_coord = tuple(co - of for co, of in zip(old_coord, start))
             out[out_coord] = val
     return out
 
 
-def apply_transform_3d(data, out, transform_coordinate, start, stop, order):
+def apply_transform_3d(data, out,
+                       transform_coordinate, sample_coordinates,
+                       start, stop):
 
     # precompute for range check
     max_range = tuple(sh - 1 for sh in data.shape)
@@ -132,13 +146,38 @@ def apply_transform_3d(data, out, transform_coordinate, start, stop, order):
                 if any(co < 0 or co >= maxr for co, maxr in zip(coord, max_range)):
                     continue
 
-                # TODO support more than nearest neighbor assignment
-                coord = tuple(int(round(co, 0)) for co in coord)
+                # get the coordinates to iterate over and the interpolation weights
+                coords, weights = sample_coordinates(coord, where_format=True)
+                # compute the output value
+                val = (weights * data[coords]).sum()
 
-                val = data[coord]
                 out_coord = tuple(co - of for co, of in zip(old_coord, start))
                 out[out_coord] = val
     return out
+
+
+# nearest neighbor sampling / order 0
+def sample_nn(coord, where_format=False):
+    if where_format:
+        return tuple(np.array([round(co, 0)], dtype='uint64') for co in coord), np.ones(1)
+    else:
+        return [tuple(int(round(co, 0)) for co in coord)], [1.]
+
+
+# TODO implement the higher orders
+# linear sampling / order 1
+def sample_linear(coord, where_format=False):
+    pass
+
+
+# quadratic sampling / order 2
+def sample_quadratic(coord, where_format=False):
+    pass
+
+
+# cubic sampling / order 3
+def sample_cubic(coord, where_format=False):
+    pass
 
 
 # TODO support multichannel transformation -> don't transform first coordinate dimension
@@ -162,8 +201,8 @@ def transform_subvolume(data, transform, bb,
         raise ValueError("Only support 2 or 3 dimensional data, not %i dimensions" % ndim)
 
     chunks = getattr(data, 'chunks', None)
-    if order > 0 or sigma is not None:
-        raise NotImplementedError()
+    if sigma is not None:
+        raise NotImplementedError("Pre-smoothing is currently not implemented.")
 
     start = tuple(b.start for b in bb)
     stop = tuple(b.stop for b in bb)
@@ -177,6 +216,13 @@ def transform_subvolume(data, transform, bb,
         _apply = apply_transform_chunked_2d if ndim == 2 else apply_transform_chunked_3d
         _apply = partial(_apply, blocking=blocking, sigma=sigma)
 
+    if order == 0:
+        sampler = sample_nn
+    # elif order == 1:
+    #     sampler = sample_linear
+    else:
+        raise NotImplementedError("Only interpolation with order <= 0 is currently implemented.")
+
     out = np.full(sub_shape, fill_value, dtype=data.dtype)
-    out = _apply(data, out, transform, start, stop, order)
+    out = _apply(data, out, transform, sampler, start, stop)
     return out
