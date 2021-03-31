3ccec5abcb9bc957705b718b21268d0e30e72322
Constantin Pape <c.pape@gmx.net>
2021-03-31 23:39:13 +0200

    Update elf.color.glasbey

--- elf/color/palette.py
+++ elf/color/palette.py
@@ -9,7 +9,7 @@ except ImportError:
     glasey_impl = None
 
 
-def glasbey(n_ids, base_palette=None, overwrite_base_palette=False,
+def glasbey(n_ids, base_palette_name, overwrite_base_palette=False,
             no_black=True, lightness_range=None, chroma_range=None,
             hue_range=None):
     """ Compute glasbey palette for maximally distant colors.
@@ -19,7 +19,7 @@ def glasbey(n_ids, base_palette=None, overwrite_base_palette=False,
 
     Arguments:
         n_ids [int] - number of ids, corresponding to entries in the palette.
-        base_palette [str] - name of the base palette.
+        base_palette_name [str] - name of the base palette.
         overwrite_base_palette [bool] -
         no_black [bool] -
         lightness_range [tuple] -
@@ -31,10 +31,11 @@ def glasbey(n_ids, base_palette=None, overwrite_base_palette=False,
 
     palette_folder = os.path.join(GLASBEY_FOLDER, 'palettes')
     palettes = glob.glob(os.path.join(palette_folder, '*.txt'))
-    palettes = {os.path.splitext(name)[0]: name for name in palettes}
-    if base_palette is not None and base_palette not in palettes:
-        raise ValueError("Invalid palette name: %s" % base_palette)
-    palette = None if base_palette is None else palettes[base_palette]
+    palettes = {os.path.splitext(os.path.split(name)[1])[0]: name for name in palettes}
+    if base_palette_name not in palettes:
+        palette_names = list(palettes.keys())
+        raise ValueError(f"Invalid palette name: {base_palette_name}, must be one of {palette_names}")
+    palette = palettes[base_palette_name]
 
     gb = glasey_impl.Glasbey(base_palette=palette, no_black=no_black,
                              overwrite_base_palette=overwrite_base_palette,
