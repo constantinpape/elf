ed5b78611597c246411327585e4fde5a41f0f506
Constantin Pape <constantin.pape@embl.de>
2021-05-26 21:04:46 +0200

    Add imports to elf.segmentation init

---
+++ elf/segmentation/__init__.py
@@ -0,0 +1,11 @@
+from .clustering import (agglomerative_clustering,
+                         cluster_segmentation, cluster_segmentation_mala,
+                         mala_clustering)
+from .features import (compute_affinity_features, compute_boundary_features, compute_boundary_mean_and_length,
+                       compute_grid_graph, compute_grid_graph_affinity_features, compute_grid_graph_image_features,
+                       compute_rag, compute_region_features,
+                       project_node_labels_to_pixels)
+from .lifted_multicut import get_lifted_multicut_solver
+from .multicut import get_multicut_solver, compute_edge_costs
+from .watershed import distance_transform_watershed, stacked_watershed
+from .workflows import edge_training, multicut_segmentation, multicut_workflow, simple_multicut_workflow
