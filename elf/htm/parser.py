850c14a37bcd1aa85db7035f289552aa8dc72db4
Constantin Pape <c.pape@gmx.net>
2022-05-22 13:32:10 +0200

    Add htm parser WIP

---
+++ elf/htm/parser.py
@@ -0,0 +1,15 @@
+
+#
+# parser functions for htm layout standards
+# outputs can be parsed directly to htm.visualization.view_plastr
+#
+
+
+# TODO
+def parse_batchlib():
+    pass
+
+
+# TODO
+def parse_mobie():
+    pass
