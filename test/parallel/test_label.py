874f48bce3d706fef6bee99a3ffc7c4142309c37
Constantin Pape <c.pape@gmx.net>
2022-12-07 22:48:58 +0100

    Fix issues with masked parallel computations

--- test/parallel/test_label.py
+++ test/parallel/test_label.py
@@ -5,7 +5,6 @@ from skimage.measure import label as label_reference
 from skimage.metrics import variation_of_information
 
 
-# TODO tests with mask
 class TestLabel(unittest.TestCase):
     def _check_labeling(self, data, res):
         exp = label_reference(data, connectivity=1)
@@ -58,6 +57,21 @@ class TestLabel(unittest.TestCase):
 
     def test_label_blobs_3d(self):
         self._test_blobs(ndim=3, size=256)
+
+    def test_label_with_mask(self):
+        from elf.parallel import label
+        data = binary_blobs(length=1024, n_dim=2, volume_fraction=0.3)
+        block_shape = (64, 64)
+
+        bb = np.s_[96:-96, 132:-87]
+        mask = np.zeros(data.shape, dtype="bool")
+        mask[bb] = 1
+
+        res = np.zeros(data.shape, dtype="uint32")
+        res = label(data, res, block_shape=block_shape, with_background=True, mask=mask)
+
+        self.assertTrue(np.allclose(res[~mask], 0))
+        self._check_labeling(data[bb], res[bb])
 
 
 if __name__ == "__main__":
--- test/parallel/test_parallel.py
+++ test/parallel/test_parallel.py
@@ -3,13 +3,12 @@ import numpy as np
 import vigra
 
 
-# TODO tests with mask
 class TestParallel(unittest.TestCase):
     def test_unique(self):
         from elf.parallel import unique
 
-        shape = 3 * (32,)
-        block_shape = 3 * (16,)
+        shape = 3 * (128,)
+        block_shape = 3 * (32,)
         x = np.random.randint(0, 2 * np.prod(shape), size=shape)
 
         u1 = unique(x, block_shape=block_shape)
@@ -21,10 +20,27 @@ class TestParallel(unittest.TestCase):
         self.assertTrue(np.array_equal(u1, u2))
         self.assertTrue(np.array_equal(c1, c2))
 
+    def test_unique_with_mask(self):
+        from elf.parallel import unique
+
+        shape = 3 * (128,)
+        block_shape = 3 * (32,)
+        x = np.random.randint(0, 2 * np.prod(shape), size=shape)
+        mask = np.random.rand(*shape) > 0.8
+
+        u1 = unique(x, block_shape=block_shape, mask=mask)
+        u2 = np.unique(x[mask])
+        self.assertTrue(np.array_equal(u1, u2))
+
+        u1, c1 = unique(x, block_shape=block_shape, return_counts=True, mask=mask)
+        u2, c2 = np.unique(x[mask], return_counts=True)
+        self.assertTrue(np.array_equal(u1, u2))
+        self.assertTrue(np.array_equal(c1, c2))
+
     def test_relabel(self):
         from elf.parallel import relabel_consecutive
-        shape = 3 * (32,)
-        block_shape = 3 * (16,)
+        shape = 3 * (128,)
+        block_shape = 3 * (32,)
         x = np.random.randint(0, 2 * np.prod(shape), size=shape).astype("uint32")
 
         xx, max_id, mapping = relabel_consecutive(x, block_shape=block_shape)
@@ -40,6 +56,24 @@ class TestParallel(unittest.TestCase):
         exp = vigra.analysis.relabelConsecutive(x)[0]
         unexp = np.unique(exp)
         self.assertTrue(np.array_equal(unx, unexp))
+
+    # FIXME
+    @unittest.expectedFailure
+    def test_relabel_with_mask(self):
+        from elf.parallel import relabel_consecutive
+        shape = 3 * (128,)
+        block_shape = 3 * (32,)
+        x = np.random.randint(0, 2 * np.prod(shape), size=shape).astype("uint32")
+        mask = np.random.rand(*shape) > 0.8
+
+        xx, max_id, mapping = relabel_consecutive(x, block_shape=block_shape, mask=mask)
+        unx = np.unique(xx[mask])
+
+        # make sure that the result is consecutive and that max_id, mapping and
+        # result are consistent
+        self.assertEqual(max_id, unx.max())
+        vals = np.array(list(mapping.values()))
+        self.assertTrue(np.array_equal(unx, vals))
 
 
 if __name__ == "__main__":
