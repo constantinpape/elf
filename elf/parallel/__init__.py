0a6c941a8ffb76b29889841a024f98e04c4d8ac1
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2023-05-30 16:41:16 +0200

    Implement parallel seeded watershed

--- elf/parallel/__init__.py
+++ elf/parallel/__init__.py
@@ -6,4 +6,5 @@ from .relabel import relabel_consecutive
 from .stats import mean, std, mean_and_std, max, min, min_and_max
 from .unique import unique
 from .label import label
+from .seeded_watershed import seeded_watershed
 from .size_filter import size_filter
