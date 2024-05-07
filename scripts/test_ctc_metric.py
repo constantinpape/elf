892ad6336cd419cb2e5a81906126b397e18f124e
Anwai Archit <anwai.archit@gmail.com>
2024-05-07 18:55:09 +0200

    Add minor refactors - to the closing logic

--- scripts/test_ctc_metric.py
+++ scripts/test_ctc_metric.py
@@ -10,9 +10,11 @@ from traccuracy._tracking_graph import TrackingGraph
 from traccuracy.metrics import CTCMetrics, DivisionMetrics
 from traccuracy.loaders._ctc import _get_node_attributes, ctc_to_graph, _check_ctc, load_ctc_data
 
+from scipy.ndimage import binary_closing
+
 
 # ROOT = "/scratch/usr/nimanwai/micro-sam/for_tracking/for_traccuracy/"  # hlrn
-ROOT = "media/anwai/ANWAI/results/micro-sam/for_traccuracy/"  # local
+ROOT = "/media/anwai/ANWAI/results/micro-sam/for_traccuracy/"  # local
 
 
 def mark_potential_split(frames, last_frame, idx):
@@ -67,7 +69,7 @@ def extract_df_from_segmentation(segmentation):
     return pred_tracks_df
 
 
-def evaluate_tracking(raw, labels, seg, segmentation_method):
+def evaluate_tracking(raw, labels, seg, segmentation_method, filter_label_ids):
     if os.path.isdir(seg):  # for trackmate stardist
         seg_T = load_ctc_data(
             data_dir=seg,
@@ -82,14 +84,19 @@ def evaluate_tracking(raw, labels, seg, segmentation_method):
         _check_ctc(seg_df, seg_nodes, seg)
         seg_T = TrackingGraph(seg_G, segmentation=seg, name=f"DynamicNuclearNet-{segmentation_method}")
 
-    breakpoint()
-
+    ids, sizes = np.unique(seg_df.Parent_ID.values, return_counts=True)
+    print(ids, sizes)
     # calcuates node attributes for each detectionc
     gt_nodes = _get_node_attributes(labels)
 
     # converts inputs to isbi-tracking format - the version expected as inputs in traccuracy
     # it's preconverted using "from deepcell_tracking.isbi_utils import trk_to_isbi"
     gt_df = pd.read_csv(os.path.join(ROOT, "gt_tracks.csv"))
+    mask = np.ones(len(gt_df), dtype="bool")
+    # breakpoint()
+    mask[np.isin(gt_df.Cell_ID, filter_label_ids)] = False
+    gt_df = gt_df[mask]
+    # breakpoint()
 
     # creates graphs from ctc-type info (isbi-type? probably means the same thing)
     gt_G = ctc_to_graph(gt_df, gt_nodes)
@@ -108,7 +115,14 @@ def evaluate_tracking(raw, labels, seg, segmentation_method):
     print(ctc_results)
 
 
-def get_tracking_data(segmentation_method):
+def size_filter(segmentation, min_size=100):
+    ids, sizes = np.unique(segmentation, return_counts=True)
+    filter_ids = ids[sizes < min_size]
+    segmentation[np.isin(segmentation, filter_ids)] = 0
+    return segmentation
+
+
+def get_tracking_data(segmentation_method, visualize=False):
     _path = os.path.join(ROOT, "tracking_micro_sam.h5")
 
     with h5py.File(_path, "r") as f:
@@ -117,17 +131,72 @@ def get_tracking_data(segmentation_method):
 
         if segmentation_method.startswith("vit"):
             segmentation = f[f"segmentations/{segmentation_method}"][:]
+            segmentation = size_filter(segmentation)
         else:
             segmentation = os.path.join(ROOT, "trackmate_stardist", "01_RES")
 
-    return raw, labels, segmentation
+    # test case
+    def check_consecutive(instances):
+        instance_ids = np.unique(instances)[1:]
+
+        id_list = []
+        for idx in instance_ids:
+            frames = np.unique(np.where(instances == idx)[0])
+            consistent_instance = (sorted(frames) == list(range(min(frames), max(frames) + 1)))
+            if not consistent_instance:
+                id_list.append(idx)
+
+        return id_list
+
+    def rectify_labels(instances):
+        id_list = check_consecutive(instances)
+        print("Closing instances", id_list)
+        for idx in id_list:
+            object_mask = (instances == idx)
+
+            structuring_element = np.zeros((3, 1, 1))
+            structuring_element[:, 0, 0] = 1
+       
+            closed_mask = binary_closing(object_mask.copy(), iterations=1, structure=structuring_element)
+            # breakpoint()
+            closed_mask = np.logical_or(object_mask, closed_mask)
+            # breakpoint()
+
+            instances[closed_mask] = idx
+
+            # import napari
+            # v = napari.Viewer()
+            # v.add_image(closed_mask.astype("uint8") * 255, name="After Closing", blending="additive", colormap="blue")
+            # v.add_image(object_mask.astype("uint8") * 255, name="Original")
+            # v.add_labels(instances, visible=False)
+            # napari.run()
+
+        return instances
+
+    filter_ids = check_consecutive(labels)
+    labels[np.isin(labels, filter_ids)] = 0
+
+    if not os.path.isdir(segmentation):
+        segmentation = rectify_labels(segmentation)
+
+    if visualize:
+        import napari
+
+        v = napari.Viewer()
+        v.add_image(raw)
+        if not os.path.isdir(segmentation):
+            v.add_labels(segmentation, visible=False)
+
+        napari.run()
+
+    return raw, labels, segmentation, filter_ids
 
 
 def main():
-    segmentation_method = "trackmate_stardist"
+    segmentation_method = "vit_l_lm"
 
-    raw, labels, segmentation = get_tracking_data(segmentation_method)
-    evaluate_tracking(raw, labels, segmentation, segmentation_method)
+    raw, labels, segmentation, filter_ids = get_tracking_data(segmentation_method, visualize=False)
+    evaluate_tracking(raw, labels, segmentation, segmentation_method, filter_ids)
 
 
 if __name__ == "__main__":
