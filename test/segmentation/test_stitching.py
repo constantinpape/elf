1ed5b84124064602f404465a9b2c32fc889a318e
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-12-29 22:50:48 +0100

    Fix issues with tile offset in segmentation stitching

--- test/segmentation/test_stitching.py
+++ test/segmentation/test_stitching.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import nifty.tools as nt
 from skimage.data import binary_blobs
 from skimage.measure import label
 
@@ -9,7 +10,7 @@ from elf.evaluation import rand_index
 
 class TestStitching(unittest.TestCase):
     def get_data(self, size=1024, ndim=2):
-        data = binary_blobs(size, blob_size_fraction=0.1, volume_fraction=0.2, n_dim=ndim)
+        data = binary_blobs(size, blob_size_fraction=0.1, volume_fraction=0.25, n_dim=ndim)
         return data
 
     def get_tiled_data(self, tile_shape, size=1024, ndim=2):
@@ -19,7 +20,6 @@ class TestStitching(unittest.TestCase):
         # Create tiles out of the data for testing label stitching.
         # Ensure offset for objects per tile to get individual ids per object per tile.
         # And finally stitch back the tiles.
-        import nifty.tools as nt
         blocking = nt.blocking([0] * ndim, data.shape, tile_shape)
         n_blocks = blocking.numberOfBlocks
 
@@ -31,8 +31,10 @@ class TestStitching(unittest.TestCase):
 
             tile = data[bb]
             tile = label(tile)
-            tile[tile != 0] += offset
-            offset = tile.max()
+            tile_mask = tile != 0
+            if tile_mask.sum() > 0:
+                tile[tile_mask] += offset
+                offset = tile.max()
 
             labels[bb] = tile
 
@@ -46,14 +48,16 @@ class TestStitching(unittest.TestCase):
             return segmentation.astype("uint32")
 
         tile_overlap = (32, 32)
-        tile_shapes = [(128, 128), (256, 256), (128, 256)]
+        tile_shapes = [(128, 128), (256, 256), (128, 256), (224, 224)]
         for tile_shape in tile_shapes:
             for _ in range(3):  # test 3 times with different data
                 data = self.get_data()
                 expected_segmentation = _segment(data)
                 segmentation = stitch_segmentation(data, _segment, tile_shape, tile_overlap, verbose=False)
+
                 are, _ = rand_index(segmentation, expected_segmentation)
-                self.assertTrue(are < 0.05)
+                # We allow for some tolerance, because small objects might get stitched incorrectly.
+                self.assertTrue(np.isclose(are, 0, rtol=1e-3, atol=1e-3))
 
     def test_stitch_segmentation_3d(self):
         from elf.segmentation.stitching import stitch_segmentation
@@ -69,7 +73,7 @@ class TestStitching(unittest.TestCase):
             expected_segmentation = _segment(data)
             segmentation = stitch_segmentation(data, _segment, tile_shape, tile_overlap, verbose=False)
             are, _ = rand_index(segmentation, expected_segmentation)
-            self.assertTrue(are < 0.05)
+            self.assertTrue(np.isclose(are, 0, rtol=1e-3, atol=1e-3))
 
     def test_stitch_tiled_segmentation(self):
         from elf.segmentation.stitching import stitch_tiled_segmentation
@@ -78,10 +82,11 @@ class TestStitching(unittest.TestCase):
         for tile_shape in tile_shapes:
             # Get the tiled segmentation with unmerged instances at tile interfaces.
             labels, original_labels = self.get_tiled_data(tile_shape=tile_shape, size=1000)
-            stitched_labels = stitch_tiled_segmentation(segmentation=labels, tile_shape=tile_shape)
+            stitched_labels = stitch_tiled_segmentation(segmentation=labels, tile_shape=tile_shape, verbose=False)
             self.assertEqual(labels.shape, stitched_labels.shape)
-            # self.assertEqual(len(np.unique(original_labels)), len(np.unique(stitched_labels)))
-            print(len(np.unique(original_labels)), len(np.unique(stitched_labels)))
+
+            are, _ = rand_index(stitched_labels, original_labels)
+            self.assertTrue(np.isclose(are, 0, rtol=1e-3, atol=1e-3))
 
 
 if __name__ == "__main__":
