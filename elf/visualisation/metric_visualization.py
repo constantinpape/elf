ab2408e78d9b035d9d7559cd412fd5a527db4a4e
Constantin Pape <constantin.pape@embl.de>
2025-05-25 20:36:54 +0200

    Update metric visualization (#126)

--- elf/visualisation/metric_visualization.py
+++ elf/visualisation/metric_visualization.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 import numpy as np
 
 from skimage.segmentation import relabel_sequential
 from elf.evaluation.matching import label_overlap, intersection_over_union
+
+from qtpy.QtWidgets import QLabel
 
 try:
     import napari
@@ -34,11 +38,11 @@ def _compute_matches(prediction, ground_truth, overlap_matrix, iou_threshold):
     fp = np.isin(prediction, fp_ids)
     fn = np.isin(ground_truth, fn_ids)
 
-    return tp, fp, fn
+    return tp, fp, fn, {"tp": tp_ids, "fp": fp_ids, "fn": fn_ids}
 
 
 def run_metric_visualization(
-    image: np.ndarray,
+    image: Optional[np.ndarray],
     prediction: np.ndarray,
     ground_truth: np.ndarray,
 ):
@@ -59,33 +63,45 @@ def run_metric_visualization(
 
     # Compute the initial TPs, FPs and FNs based on an IOU threshold of 0.5.
     iou_threshold = 0.5
-    tp, fp, fn = _compute_matches(prediction, ground_truth, overlap_matrix, iou_threshold)
+    tp, fp, fn, ids = _compute_matches(prediction, ground_truth, overlap_matrix, iou_threshold)
 
     viewer = napari.Viewer()
-    viewer.add_image(image)
+    if image is not None:
+        viewer.add_image(image)
     viewer.add_labels(ground_truth, name="Ground Truth")
     viewer.add_labels(prediction, name="Prediction")
+
+    label = QLabel()
 
     # The keyword changed from color->colormap with napari 0.5
     try:
         tp_layer = viewer.add_labels(tp, name="True Positives", color={1: "green"})
         fp_layer = viewer.add_labels(fp, name="False Positives", color={1: "red"})
-        fn_layer = viewer.add_labels(fn, name="False Negatives", color={1: "blue"})
+        fn_layer = viewer.add_labels(fn, name="False Negatives", color={1: "orange"})
     except TypeError:
         tp_layer = viewer.add_labels(tp, name="True Positives", colormap={1: "green"})
         fp_layer = viewer.add_labels(fp, name="False Positives", colormap={1: "red"})
-        fn_layer = viewer.add_labels(fn, name="False Negatives", colormap={1: "blue"})
+        fn_layer = viewer.add_labels(fn, name="False Negatives", colormap={1: "orange"})
+
+    def update_results(event=None):
+        n_tps, n_fps, n_fns = len(ids["tp"]), len(ids["fp"]), len(ids["fn"])
+        label.setText(f"Number of True Positives: {n_tps}\nNumber of False Positives: {n_fps}\nNumber of False Negatives: {n_fns}")  # noqa
 
     @magic_factory(
         call_button="Update IoU Threshold",
         iou_threshold={"widget_type": "FloatSlider", "min": 0.1, "max": 1.0, "step": 0.05}
     )
     def update_iou_threshold(iou_threshold: float = 0.5):
-        new_tp, new_fp, new_fn = _compute_matches(prediction, ground_truth, overlap_matrix, iou_threshold)
+        nonlocal ids
+        new_tp, new_fp, new_fn, ids = _compute_matches(prediction, ground_truth, overlap_matrix, iou_threshold)
         tp_layer.data = new_tp
         fp_layer.data = new_fp
         fn_layer.data = new_fn
 
+    update_results()
+    tp_layer.events.data.connect(update_results)
+
     iou_widget = update_iou_threshold()
     viewer.window.add_dock_widget(iou_widget, name="IoU Threshold Slider")
+    viewer.window.add_dock_widget(label, name="Result Overview")
     napari.run()
