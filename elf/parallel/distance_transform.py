74bc33da60e3dbc682f90083209a27bb823fe65d
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2025-06-07 10:11:42 +0200

    Implement parallel distance-based object to point mapping

--- elf/parallel/distance_transform.py
+++ elf/parallel/distance_transform.py
@@ -14,6 +14,120 @@ from tqdm import tqdm
 from .common import get_blocking
 
 
+def map_points_to_objects(
+    segmentation: ArrayLike,
+    points: np.ndarray,
+    block_shape: Tuple[int, ...],
+    halo: Optional[Tuple[int, ...]] = None,
+    sampling: Optional[Union[float, Tuple[float, ...]]] = None,
+    n_threads: Optional[int] = None,
+    verbose: bool = False,
+    roi: Optional[Tuple[slice, ...]] = None,
+) -> Tuple[np.array, np.array]:
+    """Map point coordinates to objects in a segmentation and measure the corresponding distance.
+
+    Args:
+        segmentation: The segmentation.
+        points: The point coordinates.
+        block_shape: The block shape for parallelization.
+        halo: The halo for extending the blocks in parallelization.
+            It should be chosen such that the maximum distance of interest can be computed
+            within the halo.
+        sampling: The sampling for computing the distances.
+        n_threads: The number of threads for parallelization.
+        verbose: Whether to print the progress of the computation.
+        roi: The region of interest.
+
+    Returns:
+        object_ids:
+        objetc_distances:
+    """
+
+    n_threads = multiprocessing.cpu_count() if n_threads is None else n_threads
+    blocking = get_blocking(segmentation, block_shape, roi, n_threads)
+
+    @threadpool_limits.wrap(limits=1)  # restrict the numpy threadpool to 1 to avoid oversubscription
+    def map_block(block_id):
+        if halo is None:
+            block = blocking.getBlockWithHalo(block_id, list(halo))
+        else:
+            block = blocking.getBlockWithHalo(block_id, list(halo)).outerBlock
+
+        # Get the bounding box and map the points to it.
+        bb_min, bb_max = block.begin, block.end
+        point_mask = np.logical_and.reduce(
+            [points[:, i] >= bb_min[i] for i in range(points.shape[1])] +
+            [points[:, i] < bb_max[i] for i in range(points.shape[1])]
+        )
+        # We don't need to do anything if there are no points.
+        if point_mask.sum() == 0:
+            return None
+
+        # Get the ids of the points and convert their coordinates to local coordinates
+        point_ids = np.where(point_mask)[0]
+        block_points = points[point_mask]
+        block_points -= np.array(bb_min)[None, :]
+
+        # Get the segmentation for the block.
+        bb = tuple(slice(beg, end) for beg, end in zip(bb_min, bb_max))
+        block_seg = segmentation[bb]
+
+        # Return empty assignments if the segmentation is empty.
+        if block_seg.sum() == 0:
+            object_ids = np.full(len(point_ids), 0, segmentation.dtype)
+            object_distances = np.full(len(point_ids), np.inf, "float32")
+            return point_ids, object_ids, object_distances
+
+        distances, indices = distance_transform_edt(block_seg == 0, sampling=sampling, return_indices=True)
+
+        # Index with the points and return their mapped objects and distances.
+        point_coords = tuple(block_points[:, i] for i in range(block_points.shape[1]))
+        object_distances = distances[point_coords]
+        assert block_points.shape[1] == indices.shape[0]
+        nearest_object_coord = tuple(indices[i][point_coords] for i in range(block_points.shape[1]))
+        object_ids = block_seg[nearest_object_coord]
+
+        assert len(point_ids) == len(object_distances)
+        assert len(point_ids) == len(object_ids)
+        return point_ids, object_ids, object_distances
+
+    n_blocks = blocking.numberOfBlocks
+    with futures.ThreadPoolExecutor(n_threads) as tp:
+        results = list(tqdm(
+            tp.map(map_block, range(n_blocks)), total=n_blocks,
+            desc="Map points to objects", disable=not verbose
+        ))
+
+    n_points = len(points)
+    object_ids = np.zeros(n_points, dtype=segmentation.dtype)
+    object_distances = np.full(n_points, np.inf, dtype="float32")
+
+    # Merge the results to the output arrays.
+    for res in results:
+        if res is None:
+            continue
+        this_point_ids, this_object_ids, this_distances = res
+
+        # We may have previous assignments, due to overlap introduced by the halo
+        previous_assignments = (object_ids[this_point_ids] != 0)
+        if halo is None and previous_assignments.sum() > 0:
+            raise RuntimeError("No previous assignments expected")
+        elif previous_assignments.sum() > 0:  # We do have previous assignments.
+            previous_ids, previous_distances = object_ids[this_point_ids], object_distances[this_point_ids]
+            assert len(previous_ids) == len(this_object_ids)
+            assert len(previous_distances) == len(this_distances)
+
+            # Choose the values with the minimal distance.
+            take_previous_assignment = this_distances > previous_distances
+            this_object_ids[take_previous_assignment] = previous_ids[take_previous_assignment]
+            this_distances[take_previous_assignment] = previous_distances[take_previous_assignment]
+
+        object_ids[this_point_ids] = this_object_ids
+        object_distances[this_point_ids] = this_distances
+
+    return object_ids, object_distances
+
+
 def distance_transform(
     data: ArrayLike,
     halo: Tuple[int, ...],
