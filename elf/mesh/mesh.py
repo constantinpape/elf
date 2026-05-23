ae2f0ca6b1a1349e25cd36db6003532695ce4fd2
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-05-23 08:12:14 +0200

    Migrate mesh module

--- elf/mesh/mesh.py
+++ elf/mesh/mesh.py
@@ -1,6 +1,6 @@
 from typing import Optional, Tuple
 
-import nifty
+import bioimage_cpp as bic
 import numpy as np
 from skimage.measure import marching_cubes as marching_cubes_impl
 
@@ -52,24 +52,4 @@ def smooth_mesh(
         The vertices after smoothing.
         The normals after smoothing.
     """
-    n_verts = len(verts)
-    g = nifty.graph.undirectedGraph(n_verts)
-
-    edges = np.concatenate([faces[:, :2], faces[:, 1:], faces[:, ::2]], axis=0)
-    g.insertEdges(edges)
-
-    current_verts = verts
-    current_normals = normals
-    new_verts = np.zeros_like(verts, dtype=verts.dtype)
-    new_normals = np.zeros_like(normals, dtype=normals.dtype)
-
-    # Implement this directly in nifty for speed up?
-    for it in range(iterations):
-        for vert in range(n_verts):
-            nbrs = np.array([vert] + [nbr[0] for nbr in g.nodeAdjacency(vert)], dtype="int")
-            new_verts[vert] = np.mean(current_verts[nbrs], axis=0)
-            new_normals[vert] = np.mean(current_normals[nbrs], axis=0)
-        current_verts = new_verts
-        current_normals = new_normals
-
-    return new_verts, new_normals
+    return bic.utils.smooth_mesh(verts, normals, faces, iterations)
--- test/mesh/test_mesh.py
+++ test/mesh/test_mesh.py
@@ -22,6 +22,69 @@ class TestMesh(unittest.TestCase):
             self.assertGreater(len(faces), 0)
             self.assertGreater(len(normals), 0)
 
+    def test_marching_cubes_resolution(self):
+        from elf.mesh import marching_cubes
+        shape = (32,) * 3
+        rng = np.random.default_rng(0)
+        seg = (rng.random(shape) > 0.8).astype("uint32")
+
+        verts_unit, _, _ = marching_cubes(seg, resolution=(1.0, 1.0, 1.0))
+        resolution = (2.0, 1.0, 0.5)
+        verts_scaled, _, _ = marching_cubes(seg, resolution=resolution)
+
+        self.assertEqual(verts_unit.shape, verts_scaled.shape)
+        expected = verts_unit * np.array(resolution)
+        self.assertTrue(np.allclose(verts_scaled, expected))
+
+    def test_smooth_mesh_tetrahedron(self):
+        from elf.mesh.mesh import smooth_mesh
+
+        verts = np.array(
+            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+            dtype=np.float64,
+        )
+        normals = verts + 0.5
+        faces = np.array(
+            [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]], dtype=np.int64
+        )
+
+        out_verts, out_normals = smooth_mesh(verts, normals, faces, iterations=1)
+
+        self.assertEqual(out_verts.shape, verts.shape)
+        self.assertEqual(out_normals.shape, normals.shape)
+        self.assertEqual(out_verts.dtype, verts.dtype)
+        self.assertEqual(out_normals.dtype, normals.dtype)
+
+        # Every vertex of a tetrahedron is connected to every other vertex,
+        # so one Jacobi-Laplacian iteration collapses each vertex onto the
+        # mean of all four vertices (the centroid).
+        centroid_verts = verts.mean(axis=0)
+        centroid_normals = normals.mean(axis=0)
+        for i in range(len(verts)):
+            self.assertTrue(np.allclose(out_verts[i], centroid_verts))
+            self.assertTrue(np.allclose(out_normals[i], centroid_normals))
+
+    def test_smooth_mesh_no_op(self):
+        from elf.mesh.mesh import smooth_mesh
+
+        verts = np.array(
+            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]],
+            dtype=np.float64,
+        )
+        normals = verts + 0.5
+        faces = np.array(
+            [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]], dtype=np.int64
+        )
+
+        out_verts, out_normals = smooth_mesh(verts, normals, faces, iterations=0)
+
+        self.assertEqual(out_verts.shape, verts.shape)
+        self.assertEqual(out_normals.shape, normals.shape)
+        self.assertEqual(out_verts.dtype, verts.dtype)
+        self.assertEqual(out_normals.dtype, normals.dtype)
+        self.assertTrue(np.array_equal(out_verts, verts))
+        self.assertTrue(np.array_equal(out_normals, normals))
+
 
 if __name__ == '__main__':
     unittest.main()
